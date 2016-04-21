d24c887a48187985201ea494bd9358a496774fd9
kylefoo <fung933@gmail.com>
2016-04-21 12:58:35 +0800

    unicorn.rb fixed

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,20 +1,24 @@
-# set path to application
-app_dir = File.expand_path("../..", __FILE__)
-shared_dir = "#{app_dir}/shared"
-working_directory app_dir
+# Set the working application directory
+# working_directory "/path/to/your/app"
+working_directory "/var/www/tripcanvas_hotels"
 
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+pid "/var/www/tripcanvas_hotels/shared/pids/unicorn.pid"
 
-# Set unicorn options
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+stderr_path "/var/www/tripcanvas_hotels/shared/log/unicorn.log"
+stdout_path "/var/www/tripcanvas_hotels/shared/log/unicorn.log"
+
+# Unicorn socket
+listen "/var/www/tripcanvas_hotels/shared/tmp/unicorn.tripcanvas_hotels.sock"
+listen "/var/www/tripcanvas_hotels/shared/tmp/unicorn.tripcanvas_hotels.sock"
+
+# Number of processes
+# worker_processes 4
 worker_processes 2
-preload_app true
+
+# Time-out
 timeout 30
-
-# Set up socket location
-listen "#{shared_dir}/sockets/unicorn.sock", :backlog => 64
-
-# Logging
-stderr_path "#{shared_dir}/log/unicorn.stderr.log"
-stdout_path "#{shared_dir}/log/unicorn.stdout.log"
-
-# Set master PID location
-pid "#{shared_dir}/pids/unicorn.pid"
