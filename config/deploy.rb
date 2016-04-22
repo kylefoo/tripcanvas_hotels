c2f1375b1f3f587abd4db3bfed52d11da4c68bb6
kylefoo <fung933@gmail.com>
2016-04-23 01:31:30 +0800

    try this to fix missing folders

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,7 +10,7 @@ set :rbenv_ruby, '2.3.0'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
-set :normalize_asset_timestamps, %{public/images public/javascripts public/stylesheets}
+set :normalize_asset_timestamps, false
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -52,7 +52,7 @@ namespace :deploy do
       # end
     end
   end
-  
+
   task :fix_absent_manifest_bug do
     on roles(:web) do
       within release_path do  execute :touch,
