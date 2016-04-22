6f39d2a98862d59644223ae4f46173232c9ae60b
kylefoo <fung933@gmail.com>
2016-04-23 01:23:46 +0800

    try manifest fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -52,5 +52,15 @@ namespace :deploy do
       # end
     end
   end
+  
+  task :fix_absent_manifest_bug do
+    on roles(:web) do
+      within release_path do  execute :touch,
+        release_path.join('public', fetch(:assets_prefix), 'manifest-fix.temp')
+      end
+   end
+  end
+
+  after :updating, 'deploy:fix_absent_manifest_bug'
 
 end
