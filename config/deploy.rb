7c2310234ddab24aac018c66c8736c929a3c1fc4
kylefoo <fung933@gmail.com>
2016-05-24 21:54:47 +0800

    some deployment fixed

--- config/deploy.rb
+++ config/deploy.rb
@@ -35,7 +35,7 @@ set :normalize_asset_timestamps, %{public/images public/javascripts public/style
 set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -47,11 +47,11 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
     end
   end
 
+  # task :copy_old_photos do
+  #     run "if [ -e #{previous_release}/public/uploads/* ]; then cp #{previous_release}/public/uploads/* #{current_release}/public/uploads/*; fi"
+  # end
 end
+
