1ef32c55e4ab5d5174f85a48c51057ffd7484304
kylefoo <fung933@gmail.com>
2016-06-07 01:24:39 +0800

    last resort

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -3,10 +3,10 @@ Rails.application.configure do
   config.eager_load = true
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-  config.serve_static_files = true
+  config.serve_static_files = false
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
-  config.assets.compile = false
+  config.assets.compile = true
   config.assets.digest = true
   config.log_level = :info
   config.i18n.fallbacks = true
