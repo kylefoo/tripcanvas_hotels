ce003b2183328ef8a28582d0b8bceec0265ec620
kylefoo <fung933@gmail.com>
2016-06-06 20:53:42 +0800

    aplication.rb updated

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,12 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+require "active_model/railtie"
+require "active_record/railtie"
+require "action_controller/railtie"
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "sprockets/railtie"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -22,5 +27,6 @@ module TripcanvasHotels
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.enabled = true
   end
 end
