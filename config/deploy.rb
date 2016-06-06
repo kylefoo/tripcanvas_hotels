2fc324fbf960c8ea4ee688b91c6db52687fb7937
kylefoo <fung933@gmail.com>
2016-06-07 00:45:50 +0800

    upgraded ruby version

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,7 +6,7 @@ set :repo_url, 'git@github.com:kylefoo/tripcanvas_hotels.git'
 
 set :rbenv_path, '~/.rbenv'
 set :rbenv_type, :system
-set :rbenv_ruby, '2.3.0'
+set :rbenv_ruby, '2.3.1'
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
 set :rbenv_map_bins, %w{rake gem bundle ruby rails}
 set :rbenv_roles, :all # default value
