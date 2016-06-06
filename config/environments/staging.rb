2589721c303d9ea7347d5a646004e5821c12ca54
kylefoo <fung933@gmail.com>
2016-06-06 16:12:27 +0800

    how about this

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -5,7 +5,7 @@ Rails.application.configure do
   config.action_controller.perform_caching = true
   config.serve_static_files = false
   config.assets.js_compressor = :uglifier
-  config.assets.compile = true
+  config.assets.compile = false
   config.assets.digest = true
   config.log_level = :info
   config.i18n.fallbacks = true
