6f31811559c373630b840c0ee98d27bb731a5447
kylefoo <fung933@gmail.com>
2016-06-19 17:31:37 +0800

    allowed divs

--- app/assets/javascripts/redactor-rails/config.js
+++ app/assets/javascripts/redactor-rails/config.js
@@ -9,6 +9,7 @@ window.init_redactor = function(){
         // You can specify, which ones plugins you need.
         // If you want to use plugins, you have add plugins to your
         // application.js and application.css files and uncomment the line below:
+        "replaceDivs": false,
         "plugins": ['fontsize', 'fontcolor', 'fontfamily', 'fullscreen', 'textdirection', 'clips', 'source'],
         "imageUpload":"/redactor_rails/pictures?" + params,
         "imageGetJson":"/redactor_rails/pictures",
